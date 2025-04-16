21ba6c5253eb93346cc58f93ccf267962ed8f53f
jhaykrab <jakecasundo15@gmail.com>
2025-04-17 01:10:41 +0800

    Initial Commit: Setup OOPPortfolioWeb project

---
+++ OOPPortfolioWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OOPPortfolioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
